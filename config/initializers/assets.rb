a1f7bb8bdcb7d91370ca50060bff21ae8a4c89f5
Eduard  Berrio <eduard@kommit.co>
2015-01-22 10:53:58 -0500

    styeles aplication

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( stylish-portfolio.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
